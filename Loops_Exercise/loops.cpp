d9b0362e3f7b614e8fa5a2e66bfc017eab609037
Darien LeLeux <austinblackofficial@gmail.com>
2016-08-31 16:00:30 -0500

    Second Commit
    Did some work.

--- Loops_Exercise/loops.cpp
+++ Loops_Exercise/loops.cpp
@@ -48,23 +48,35 @@ int main()
 				- For multiples of 3 and 5 print “FizzBuzz”
 				- Otherwise print the number.
 				Note, you will need to use the modulus operator (%)*/
-		{
-			int loop = 100;
-			for (int i = 0; i < 101; i++)
-			std::cout << i << std::endl;
-				if (i % 3 == 0)
-				{
-					printf("Fizz");
-				}
-				else if (i % 5 == 0)
-				{
-					printf("Buzz");
-				}
-				else if (i % 5 || i % 3 == 0)
-				{
-					printf("FizzBuzz");
-				}
-		}
+		//{
+		//	int loop = 100;
+		//	for (int i = 0; i < 101; i++)
+		//	std::cout << i << std::endl;
+
+		//		if (i % 3 == 0)
+		//		{
+		//			printf("Fizz");
+		//		}
+		//		else if (i % 5 == 0)
+		//		{
+		//			printf("Buzz");
+		//		}
+		//		else if (i % 5 || i % 3 == 0)
+		//		{
+		//			printf("FizzBuzz");
+		//		}
+		//}
+
+	//3
+
+	//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum
+	//of these multiples is 23.
+
+	//Find the sum of all the multiples of 3 or 5 below 1000.
+
+	//4
+
+	/*Complete the above 3 exercises using a “while” loop.*/
 
 	system("pause");
 	return 0;
